6ee256b56095b629c7e1baf5371d7f73fef4bc7d
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-02-01 16:38:09 +0000

    new adventure page component created with form to create a new trip

---
+++ src/pages/NewAdventure.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateAdventureForm from '../components/CreateAdventureForm'
+
+const NewAdventure = () => {
+    return (
+        <section id="new-adventure">
+            <CreateAdventureForm title="Create a new adventure"/>
+        </section>
+    )
+}
+
+export default NewAdventure
