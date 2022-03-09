89f79fde33c39ec03a67de674bf07d1e0f4c170b
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-03-09 16:13:34 +0000

    project task list updated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import NewAdventures from './pages/NewAdventure';
 
 // *** Remember - best practice in React is to lift the state up in the component hierarchy to the parent component - in which my case is most likey to be the page components
 
-  // 1 - App grid css layout - reusable CSS Grid for page, flex for components
+  // 1 - ✔ App grid css layout - reusable CSS Grid for page, flex for components
   // 2 - ✔ Page components
   // 3 - ✔ Components - Parent & child & reusing components with filtering results of the props data passed in with css for each
   // 4 - ✔ JSON Server to store data using fetchapi & use affect to GET, POST & DELETE & error handling
@@ -19,9 +19,9 @@ import NewAdventures from './pages/NewAdventure';
   // 6 - CRUD operations on adventures - ✔ GET,  ✔ POST, PATCH, DELETE
   // 7 - ✔ Component Form to create adventures with http POST and event to onsubmit and onchange of input box
   // 8 - Conditional template rendering - favorites, catagories & Example loading & errors, css classes, updating/setting state. Process is to set the state up and use conditional rendering with functions to change the state
-  // 9 - Event handlers for conditional rendering using state , clicks, submit forms, toggling
+  // 9 -  ✔ Event handlers for conditional rendering using state , clicks, submit forms, toggling
   // 10 - ✔ Decide were to store the core logic and usEffects for parent/child components 
-  // 11 - Stateful components including changes to state depending upon interaction.
+  // 11 - ✔ Stateful components including changes to state depending upon interaction.
   // 12 - ✔ Components should have a single functionality without to much complexity 
   // 13 - ✔ Local data vs server data and how to CRUD that data
   // 14 - ✔ Props - functions, data
@@ -36,7 +36,7 @@ import NewAdventures from './pages/NewAdventure';
   // 23 - Lift up state and functions in the createAdventure form to the parent component, createAdventure page. Pass then state and functions in to the form as props.
   // 24 - Alert component - show when new record created 
   // 25 - Add redirect and form cleanup on the createAdventureForm component 
-  // 26 - convert CRUD operation from fetch to async await requests
+  // 26 - ✔ Convert CRUD operation from fetch to async await requests
 
 function App() {
 
